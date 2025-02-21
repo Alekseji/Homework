071e011d01510ea8b9d6629fcd9f526f46c6d745
Alekseji <118532252+Alekseji@users.noreply.github.com>
2025-02-21 11:52:46 +0400

    Add files via upload

---
+++ Homework.cpp
@@ -0,0 +1,66 @@
+// Введите целое число или число '0', чтобы закончить:
+// 13
+// Введите целое число или число '0', чтобы закончить:
+// 666
+// Введите целое число или число '0', чтобы закончить:
+// 42
+// Введите целое число или число '0', чтобы закончить:
+// 0
+// Сумма: 721
+// Задание 1
+// #include <iostream>
+// #include <string>
+// int main()
+// {
+// int s=0;
+// int sum=0;
+
+// do {
+//     std::cout<<"Введите целое число или число'0'"<<std::endl;
+//     std::cin>>s;
+//     sum+=s;
+
+// }while(s!=0);
+
+// std::cout<<"Сумма:"<<sum<<std::endl;
+
+// return 0;
+// }
+// #include <iostream>
+// #include <string>
+// int main()
+// {
+// int a=0;
+
+// std::cout<<"Введите целое число"<<std::endl;
+// std::cin>>a; 
+// int sum=0;
+
+// while (a!=0)
+// {
+    
+//     sum+=a%10;
+//     a/=10;            
+// }
+// std::cout<<"Сумма:"<<sum<<std::endl;
+
+// return 0;
+// }
+// Задание 3
+// #include <iostream>
+// #include <string>
+// int main()
+// {
+// int variable {};
+// std::cin>>variable;
+// int limit {10};
+
+// for(int counter{1};counter<=10;++counter)
+// {
+//     std::cout<<variable*counter<<std::endl;
+       
+   
+// }
+  
+//     return 0;
+// }
